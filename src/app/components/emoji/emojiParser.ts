cdd6f9f460eb68d64c9b2a35424cc4e91fbd4051
Marvin <Mvin@live.de>
2024-08-11 13:36:54 +0200

    Updated colors

--- src/app/components/emoji/emojiParser.ts
+++ src/app/components/emoji/emojiParser.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HookParser, HookValue, HookComponentData, HookBindings, HookFinder, HookPosition } from 'ngx-dynamic-hooks';
+import { HookParser, HookValue, HookComponentData, HookBindings, HookFinder, HookPosition, ParseOptions } from 'ngx-dynamic-hooks';
 import { EmojiComponent } from './emoji.component';
 
 @Injectable({ 
@@ -9,22 +9,20 @@ export class EmojiParser implements HookParser {
 
   constructor(private hookFinder: HookFinder) {}
 
-  public findHooks(content: string, context: any): HookPosition[] {
+  findHooks(content: string, context: any, options: ParseOptions): HookPosition[] {
     // As an example, this regex finds the emoticons :-D, :-O and :-*
     const emoticonRegex = /(?::-D|:-O|:-\*)/gm;
 
     // We can use the HookFinder service provided by the library to easily
     // find the HookPositions of any regex in the content string
-    return this.hookFinder.findSingletagHooks(content, emoticonRegex);
+    return this.hookFinder.find(content, emoticonRegex);
   }
 
-  public loadComponent(hookId: number, hookValue: HookValue, context: any, childNodes: Array<Element>): HookComponentData {
-    return {
-      component: EmojiComponent
-    };
+  loadComponent(hookId: number, hookValue: HookValue, context: any, childNodes: Element[], options: ParseOptions): HookComponentData {
+    return { component: EmojiComponent };
   }
 
-  public getBindings(hookId: number, hookValue: HookValue, context: any): HookBindings {
+  getBindings(hookId: number, hookValue: HookValue, context: any, options: ParseOptions): HookBindings {
     // Lets see what kind of emoticon this hook is and assign a fitting emoji
     let emojiType: string;
     switch (hookValue.openingTag) {
